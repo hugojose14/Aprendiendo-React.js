78f293442fa3ad1cb4593415ebc2676b6c32ca55
hugojose144 <hugo.perez@zabud.com.co>
2020-05-09 19:31:35 -0500

    importando useState el cual me va a permitir tener state's en mi aplicación.

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -1,8 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  //Lista de productos
+  //El primero va a ser el state y el segundo una función que me ayuda rescribirlo
+  //Dentro del useState los valores iniciales
+  const [products, guardarProducts] = useState([
+    { id: 1, nombre: "Camisa JS", precio: 150 },
+    { id: 2, nombre: "Camisa Python", precio: 0 },
+    { id: 3, nombre: "Camisa Go", precio: 80 },
+    { id: 4, nombre: "Camisa Java", precio: 100 },
+  ]);
+
   //Obtener la fecha
   const fecha = new Date().getFullYear();
 
